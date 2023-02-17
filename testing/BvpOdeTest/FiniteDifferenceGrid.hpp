5273eb8c43785aab7fdb9d6a4a5c26f91d8b1bd1
Matthew Lintern-Monro <86484124+MRLintern@users.noreply.github.com>
2023-02-17 00:41:50 +0000

    Updated test files

---
+++ testing/BvpOdeTest/FiniteDifferenceGrid.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <vector>
+#include "Node.hpp"
+
+class FiniteDifferenceGrid
+{
+public:
+   // The boundary value class is able to
+   // access the nodes
+   friend class BvpOde;
+private:
+   std::vector<Node> mNodes;
+public:
+   FiniteDifferenceGrid(int numNodes, double xMin, 
+                        double xMax);
+};
+
