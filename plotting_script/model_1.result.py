11d34bc9727c4aeeaf4032027a83bb7574e184de
Matthew Lintern-Monro <86484124+MRLintern@users.noreply.github.com>
2023-02-20 09:55:16 +0000

    Initial Commit: change file name to produce graphs for other results

---
+++ plotting_script/model_1.result.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+
+X, Y = [], []
+
+for line in open('model_problem_results1.dat', 'r'):
+
+	values = [float(s) for s in line.split()]
+	X.append(values[0])
+	Y.append(values[1])
+	
+plt.plot(X,Y)
+plt.show()
+
+
+
