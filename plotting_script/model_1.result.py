42ad0bd89ff04c11365e56cfc7e8336d096930b6
Matthew Lintern-Monro <86484124+MRLintern@users.noreply.github.com>
2023-02-20 12:19:54 +0000

    script updated; redefined var names

--- plotting_script/model_1.result.py
+++ plotting_script/model_1.result.py
@@ -1,17 +1,17 @@
 import matplotlib.pyplot as plt
 
-X, Y = [], []
+x, u = [], []
 
 for line in open('model_problem_results1.dat', 'r'):
 
 	values = [float(s) for s in line.split()]
-	X.append(values[0])
-	Y.append(values[1])
+	x.append(values[0])
+	u.append(values[1])
 	
 plt.xlabel('x')
 plt.ylabel('u(x)')
 plt.title('Sollution to Model')
-plt.plot(X,Y)
+plt.plot(x,u)
 plt.show()
 
 
