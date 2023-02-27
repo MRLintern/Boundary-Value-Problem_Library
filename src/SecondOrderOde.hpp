bba6cd47c69e190692092a8376481a8e9eeb6c2d
Matthew Lintern-Monro <86484124+MRLintern@users.noreply.github.com>
2023-02-27 19:46:00 +0000

    Comments added

--- src/SecondOrderOde.hpp
+++ src/SecondOrderOde.hpp
@@ -5,22 +5,29 @@ class SecondOrderOde
    // The boundary value class is able to
    // access the coefficients etc. of this equation
    friend class BvpOde;
+   
 private:
+   
    // Coefficients on LHS of ODE
    double mCoeffOfUxx;
    double mCoeffOfUx;
    double mCoeffOfU;
+   
    // Function on RHS of ODE
    double (*mpRhsFunc)(double x);
 
    // Interval for domain
    double mXmin;
    double mXmax;
+   
 public:
+   
+   //constructor for the 2nd order ODE initialises the coefficients of all derivates, the source function and the start and end of interval
    SecondOrderOde(double coeffUxx, double coeffUx, 
                   double coeffU, 
                   double (*righthandSide)(double),
                   double xMinimum, double xMaximum)
+      
    {
       mCoeffOfUxx = coeffUxx;
       mCoeffOfUx = coeffUx;
