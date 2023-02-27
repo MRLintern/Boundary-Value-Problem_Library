8e0cd7971149351bdf3b9af744410e6332f56af8
Matthew Lintern-Monro <86484124+MRLintern@users.noreply.github.com>
2023-02-27 13:56:19 +0000

    Comments added

--- src/FiniteDifferenceGrid.cpp
+++ src/FiniteDifferenceGrid.cpp
@@ -2,15 +2,22 @@
 #include "FiniteDifferenceGrid.hpp"
 #include "Node.hpp"
 
-FiniteDifferenceGrid::FiniteDifferenceGrid(int numNodes, 
-                                  double xMin, double xMax)
+//initialise the number of nodes and  the min/max points on the interval; x_min < x < x_max
+FiniteDifferenceGrid::FiniteDifferenceGrid(int numNodes, double xMin, double xMax)
 {
+   //size/difference between each node a long the interval
    double stepsize = (xMax-xMin)/((double)(numNodes-1));
+  
    for (int i=0; i<numNodes; i++)
    {
+      //create node instance
       Node node;
+      //coordinates for nodes on interval
       node.coordinate = xMin+i*stepsize;
+      //add nodes
       mNodes.push_back(node);
    }
+  
+   //check number of nodes
    assert(mNodes.size() == numNodes);
 }
