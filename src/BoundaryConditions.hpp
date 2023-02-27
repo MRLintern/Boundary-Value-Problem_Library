b468af46137bac7e6bdec8ff5f3ce92400ba10eb
Matthew Lintern-Monro <86484124+MRLintern@users.noreply.github.com>
2023-02-27 13:18:21 +0000

    Comments added

--- src/BoundaryConditions.hpp
+++ src/BoundaryConditions.hpp
@@ -1,22 +1,57 @@
+/*
+
+Boundary conditions (BCs) class. Two types used here. 1) Dirichlet and 2) Neumann
+
+1) Dirichlet: Dirichlet boundary conditions assume the solution to the variable
+2) Neumann: Neumann boundary conditions, a solution is assumed for the derivative of the variable.
+
+BC: both types are used on the left and right hand side of interval.
+
+*/
+
+
 #pragma once
 
 class BoundaryConditions
 {
 public:
+
    // The boundary value class is able to
    // access the coefficients etc. of this equation
    friend class BvpOde;
+   
 private:
+
+   //--Dirichlet BCs--//
+   
+   //LHS Dirichlert
    bool mLhsBcIsDirichlet;
+   //RHS Dirichlet
    bool mRhsBcIsDirichlet;
+   
+   //--Neumann BCs--//
+   
+   //LHS Neumann
    bool mLhsBcIsNeumann;
+   //RHS Neumann
    bool mRhsBcIsNeumann;
+   
+   //variables used to store values for BCs
    double mLhsBcValue;
    double mRhsBcValue;
+   
 public:
+   
+   //initialising 
    BoundaryConditions();
+   
+   //--set the BCs with values--//
+   
+   //Dirichlet
    void SetLhsDirichletBc(double lhsValue);
    void SetRhsDirichletBc(double rhsValue);
+   
+   //Neumann
    void SetLhsNeumannBc(double lhsDerivValue);
    void SetRhsNeumannBc(double rhsDerivValue);
 };
