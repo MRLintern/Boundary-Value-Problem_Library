61237180d3cf7be2278369c053084104b5289877
Matthew Lintern-Monro <86484124+MRLintern@users.noreply.github.com>
2023-02-27 13:24:44 +0000

    Comments added

--- src/BoundaryConditions.cpp
+++ src/BoundaryConditions.cpp
@@ -1,5 +1,15 @@
+/*
+
+Initialise BCs 
+
+Note: if using Dirichlet BCs we can't use Neumann
+      if Neumann BCs are sued, we can't use Dirchlet
+
+
 #include <cassert>
 #include "BoundaryConditions.hpp"
+
+//Initially BCs are not true; values will be set
 
 BoundaryConditions::BoundaryConditions()
 {
@@ -8,6 +18,8 @@ BoundaryConditions::BoundaryConditions()
    mLhsBcIsNeumann = false;
    mRhsBcIsNeumann = false;
 }
+
+//Initialise LHS and RHS Dirichlet BCs
 
 void BoundaryConditions::SetLhsDirichletBc(double lhsValue)
 {
@@ -23,16 +35,16 @@ void BoundaryConditions::SetRhsDirichletBc(double rhsValue)
    mRhsBcValue = rhsValue;
 }
 
-void BoundaryConditions::
-          SetLhsNeumannBc(double lhsDerivValue)
+//Initial LHS and RHS Neumann BCs
+
+void BoundaryConditions::SetLhsNeumannBc(double lhsDerivValue)
 {
    assert(!mLhsBcIsDirichlet);
    mLhsBcIsNeumann = true;
    mLhsBcValue = lhsDerivValue;
 }
 
-void BoundaryConditions::
-          SetRhsNeumannBc(double rhsDerivValue)
+void BoundaryConditions::SetRhsNeumannBc(double rhsDerivValue)
 {
    assert(!mRhsBcIsDirichlet);
    mRhsBcIsNeumann = true;
